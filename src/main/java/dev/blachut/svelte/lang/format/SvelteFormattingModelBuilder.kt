3a2b8a934c5502962113db88a5eb48656c3e7729
Tomasz Błachut <blachut.tomasz@gmail.com>
2020-01-11 13:05:17 +0100

    Create SvelteFormattingModelBuilder

---
+++ src/main/java/dev/blachut/svelte/lang/format/SvelteFormattingModelBuilder.kt
@@ -0,0 +1,24 @@
+package dev.blachut.svelte.lang.format
+
+import com.intellij.formatting.FormattingModel
+import com.intellij.formatting.FormattingModelBuilder
+import com.intellij.lang.xml.XmlFormattingModel
+import com.intellij.psi.PsiElement
+import com.intellij.psi.codeStyle.CodeStyleSettings
+import com.intellij.psi.formatter.FormattingDocumentModelImpl
+import com.intellij.psi.formatter.xml.HtmlPolicy
+import com.intellij.psi.formatter.xml.XmlBlock
+import com.intellij.psi.impl.source.SourceTreeToPsiMap
+
+class SvelteFormattingModelBuilder : FormattingModelBuilder {
+    override fun createModel(element: PsiElement, settings: CodeStyleSettings): FormattingModel {
+        val psiFile = element.containingFile
+        val documentModel = FormattingDocumentModelImpl.createOn(psiFile)
+
+        val astNode = SourceTreeToPsiMap.psiElementToTree(psiFile)
+        val formattingPolicy = HtmlPolicy(settings, documentModel)
+        val block = XmlBlock(astNode, null, null, formattingPolicy, null, null, false)
+
+        return XmlFormattingModel(psiFile, block, documentModel)
+    }
+}
